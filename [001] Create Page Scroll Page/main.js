0c7b89502c89fdaa5925a9476516a304c24b7363
Safwan <sfwnisme@gmail.com>
2022-12-15 17:56:39 +0300

    .

---
+++ [001] Create Page Scroll Page/main.js
@@ -0,0 +1,8 @@
+const scroller = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  scroller.style.width = `${(scrollTop / height) * 100}%`;
+});
