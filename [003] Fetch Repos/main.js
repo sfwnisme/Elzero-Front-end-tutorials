301285ca39f672e89eee051a60e0ca580e76ceca
Safwan <sfwnisme@gmail.com>
2022-12-17 14:57:10 +0300

    .

--- [003] Fetch Repos/main.js
+++ [003] Fetch Repos/main.js
@@ -1,27 +1,76 @@
-let text = document.querySelector("[type= text]");
-let btn = document.querySelector(".get-button");
-let showData = document.querySelector(".show-data");
+let theInput = document.querySelector("[type= text]");
+let getButton = document.querySelector(".get-button");
+let repoData = document.querySelector(".show-data");
 
-function clickOnBtn(e) {
-  e.addEventListener("click", () => {
-    addText(text.value);
-    text.value = "";
-  });
-}
-clickOnBtn(btn);
-
-function addText(textValue) {
-  let repoBox = document.createElement("div");
-  repoBox.classList.add("repo-box");
-
-  let span = document.createElement("span");
-  span.innerText = textValue;
-  async function fet() {
-    let data = await fetch("GET", `https://api.github.com/users/${textValue}`);
-    console.log(await data.json());
-    data.then((result) => console.log(result))
+// using 'Enter' key on the keyboard to implement the function
+theInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    getRepos();
+    event.value = "";
   }
-  fet()
-  repoBox.append(span);
-  document.body.append(repoBox);
+});
+
+// on click implement the function
+getButton.onclick = function () {
+  getRepos();
+  theInput.value = "";
+};
+
+function getRepos() {
+  if (theInput.value == "") {
+    console.log("Input empty");
+    repoData.innerHTML = "<span>Please Write Github Username</span>";
+  } else {
+    console.log(theInput.value);
+    fetch(`https://api.github.com/users/${theInput.value}/repos`)
+      .then((response) => response.json())
+      .then((repositories) => {
+        console.log(repositories);
+
+        //empty the container
+        repoData.innerHTML = "";
+        //loop on repositories
+        repositories.forEach((repo) => {
+          //create the main div element
+          let mainDiv = document.createElement("div");
+          mainDiv.className = "main-div";
+
+          // create Repo name Text
+          let repoName = document.createElement("span");
+          let repoNameText = document.createTextNode(repo.name);
+          repoName.append(repoNameText);
+          // Append The Text To Main Div
+          mainDiv.append(repoName);
+
+          // Create Links Container
+          let links = document.createElement("div");
+          links.className = "links";
+
+          // Creat link
+          let link = document.createElement("a");
+          link.innerHTML = "visit";
+          link.href = repo.html_url;
+          link.setAttribute("target", `_blank`);
+
+          //append link To Links container
+          links.append(link);
+
+          // stars
+          let stars = document.createElement("span");
+          let starsText = document.createTextNode(
+            `${repo.stargazers_count}.stars`
+          );
+          stars.append(starsText);
+
+          // append stars to links container
+          links.append(stars);
+
+          //append links To mainDiv
+          mainDiv.append(links);
+
+          // Append The Main Div To Container
+          repoData.append(mainDiv);
+        });
+      });
+  }
 }
