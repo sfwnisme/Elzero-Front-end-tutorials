145b6015ee35a6418c613fe005094e3d116873e6
Safwan <sfwnisme@gmail.com>
2023-01-15 11:13:26 +0300

    .

---
+++ [011] cursor kit/main.js
@@ -0,0 +1,53 @@
+/* =====[CURSOR ELEMENTS]===== */
+// cursor box
+let cursorBox = document.createElement("div");
+cursorBox.className = "cursor-box";
+
+// cursor elements
+let cursorOne = document.createElement("div");
+cursorOne.className = "cursor-one";
+let cursorTwo = document.createElement("div");
+cursorTwo.className = "cursor-two";
+
+//append childs
+cursorBox.appendChild(cursorOne);
+cursorBox.appendChild(cursorTwo);
+// append
+document.body.append(cursorBox);
+/* =============================== */
+
+/* =====[CURSOR EVENTS]===== */
+function cursorBOM(first, second) {
+  document.addEventListener("mousemove", (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+
+    first.style.left = second.style.left = `${x}px`;
+    first.style.top = second.style.top = `${y}px`;
+  });
+}
+cursorBOM(cursorOne, cursorTwo);
+/* =============================== */
+
+/* =====[CURSOR AUTO FUNCTION]===== */
+let allbtns = document.querySelectorAll("button");
+let allLinks = document.querySelectorAll("a");
+
+addAndRemove(allbtns, cursorOne, cursorTwo);
+addAndRemove(allLinks, cursorOne, cursorTwo);
+
+function addAndRemove(element, one, two) {
+  element.forEach((ele) => {
+    ele.addEventListener("mouseenter", (e) => {
+      one.classList.add("active");
+      two.classList.add("disable");
+    });
+  });
+  element.forEach((ele) => {
+    ele.addEventListener("mouseleave", (e) => {
+      one.classList.remove("active");
+      two.classList.remove("disable");
+    });
+  });
+}
+/* =============================== */
